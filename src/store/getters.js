687e990d31f097119b54f5d515989f797f8a5440
liuwendan <18827389887@163.com>
2021-02-24 09:05:32 +0800

    第一次提交

---
+++ src/store/getters.js
@@ -0,0 +1,17 @@
+/** 
+ * Vuex.Store getters
+ * @author LiQingSong
+ */
+const getters = {
+  siteFiexdHeader: state => state.app.siteFiexdHeader,
+  siteTopNavEnable: state => state.app.siteTopNavEnable,
+  siteSidebarLogo: state => state.app.siteSidebarLogo,
+  sidebarOpened: state => state.app.sidebarOpened,
+  token: state => state.user.token,
+  avatar: state => state.user.avatar,
+  msgtotal: state => state.user.msgtotal,
+  name: state => state.user.name,
+  roles: state => state.user.roles,
+  permission_routes: state => state.permission.routes
+};
+export default getters;
